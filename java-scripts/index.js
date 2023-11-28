3e1a5676284b63637e00eb610633a50103184858
Miigaa <miigaa@Miigaas-MacBook-Air.local>
2023-11-28 14:23:22 +0800

    js - bodolt

---
+++ java-scripts/index.js
@@ -0,0 +1,5 @@
+let number = 999;
+let firstNum = parseInt(number / 100);
+let secondNum = parseInt((number / 10) % 10);
+let thirdNum = parseInt(number % 10);
+console.log(firstNum + secondNum + thirdNum);
