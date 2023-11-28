a8c909a98fd931c2b1e204577f25af458e6bc57f
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-11-28 19:08:50 +0800

    js-first

--- java-scripts/index.js
+++ java-scripts/index.js
@@ -1,5 +1,49 @@
-let number = 999;
-let firstNum = parseInt(number / 100);
-let secondNum = parseInt((number / 10) % 10);
-let thirdNum = parseInt(number % 10);
-console.log(firstNum + secondNum + thirdNum);
+// let number = 999;
+// let firstNum = parseInt(number / 100);
+// let secondNum = parseInt((number / 10) % 10);
+// let thirdNum = parseInt(number % 10);
+// console.log(firstNum + secondNum + thirdNum);
+
+// if (number % 2 === 0) {
+//   if (number > 10) {
+//     console.log("tegsh too bna 10-aas ih ");
+//   } else {
+//     console.log("tegsh too bna 10-aas baga ");
+//   }
+// } else {
+//   if (number > 10) {
+//     console.log("sondgoi too 10-aas ih");
+//   } else {
+//     console.log("sondgoi too 10-aas baga");
+//   }
+// }
+
+// if (number % 2 === 0 && number > 10) {
+//   console.log("tegsh too ba 10 aas ih ");
+// } else if (number % 2 === 0 && number < 10) {
+//   console.log("tegsh too ba 10 - aas baga");
+// } else if (number % 2 !== 0 && number < 10) {
+//   console.log("sondgoi too ba 10-aas baga");
+// } else {
+//   console.log("songoi too ba 10 - aas ih");
+// }
+
+// let number = 3;
+// if (number === 1) {
+//   console.log("Monday");
+// } else if (number === 2) {
+//   console.log("Tuesday");
+// } else if (number === 3) {
+//   console.log("Wednesday");
+// } else if (number === 4) {
+//   console.log("Thursday");
+// } else if (number === 5) {
+//   console.log("Friday");
+// } else if (number === 6) {
+//   console.log("Saturday");
+// } else if (number === 7) {
+//   console.log("Sunday");
+// }
+1 false
+2 true 
+3 true
