08af6560c6b4604ce76e417591e79bde997a53a2
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-11-29 19:27:36 +0800

    js

--- java-scripts/index.js
+++ java-scripts/index.js
@@ -4,6 +4,8 @@
 // let thirdNum = parseInt(number % 10);
 // console.log(firstNum + secondNum + thirdNum);
 
+// 10aas ih too ba baga toog shalgah
+
 // if (number % 2 === 0) {
 //   if (number > 10) {
 //     console.log("tegsh too bna 10-aas ih ");
@@ -28,6 +30,8 @@
 //   console.log("songoi too ba 10 - aas ih");
 // }
 
+//doloo honogiin odoruud
+
 // let number = 3;
 // if (number === 1) {
 //   console.log("Monday");
@@ -44,6 +48,110 @@
 // } else if (number === 7) {
 //   console.log("Sunday");
 // }
-1 false
-2 true 
-3 true
+
+//eyreg sorog oloh
+
+// let number = -23;
+// if (number < 0) {
+//   console.log("sorog too bna ");
+// } else {
+//   console.log("eyreg too ");
+// }
+
+//3n tooni ihiig oloh
+
+// let firstNum = 213;
+// let secondNum = 32;
+// let thirdNum = 34;
+// if (firstNum < secondNum) {
+//   if (secondNum < thirdNum) {
+//     console.log(thirdNum);
+//   } else {
+//     console.log(secondNum);
+//   }
+// } else {
+//   if (firstNum > thirdNum) {
+//     console.log(firstNum);
+//   } else {
+//     console.log(thirdNum);
+//   }
+// }
+
+// jingiin index oloh
+
+// let height = 175;
+// let weight = 70;
+// let secondHeight = (height / 100) ** 2;
+// index = weight / secondHeight;
+// console.log(index);
+// if (index < 18.5) {
+//   console.log("Jingiin dutagdaltai");
+// } else if (index >= 18.8 && index < 24.9) {
+//   console.log("eruul");
+// } else if (index >= 24.9 && index < 30) {
+//   console.log("iluudel jintei");
+// } else {
+//   console.log("het targan bna turaach");
+// }
+
+//switch case
+
+// let day = 8;
+// let dayName;
+// switch (day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   case 4:
+//     dayName = "Thursday";
+//     break;
+//   case 5:
+//     dayName = "Friday";
+//     break;
+//   case 6:
+//     dayName = "Saturday";
+//     break;
+//   case 7:
+//     dayName = "Sunday";
+//     break;
+//   default:
+//     dayName = "Tiim odor bhgui ";
+//     break;
+// }
+// console.log(dayName);
+
+// let person = "Miigaa";
+// let irsenHumuus;
+// switch (person) {
+//   case "Naraa":
+//     irsenHumuus = "Naraa";
+//     break;
+//   case "Baldan":
+//     irsenHumuus = "Baldan - irlee";
+//     break;
+//   case "Dulmaa":
+//     irsenHumuus = "Dulmaa - irlee";
+//     break;
+//   case "Galt":
+//     irsenHumuus = "Galt - irlee";
+//     break;
+//   case "Goldon":
+//     irsenHumuus = "Goldon - irlee";
+//     break;
+//   default:
+//     irsenHumuus = "Tm hun bhgui";
+//     break;
+// }
+// console.log(irsenHumuus);
+
+//TERNARY
+
+// let age = 18;
+// let info = age >= 18 ? "Nasand hursen bna " : "Nasand hureegui bna ";
+// console.log(info);
