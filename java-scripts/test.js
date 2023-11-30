9fd0ece97f0f8be0e4727225c336f84787a5f649
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-11-30 19:25:03 +0800

    asdf

--- java-scripts/test.js
+++ java-scripts/test.js
@@ -1,1 +1,7 @@
+let averageA = (96 + 108 + 89) / 3;
 
+let averageB = (88 + 91 + 110) / 3;
+averageA > averageB
+  ? console.log("A - bag turuulsen")
+  : console.log("B - bag turuulsen");
+averageA = averageB ? console.log("baguudin onoo tentsej") : faskdlfj;
