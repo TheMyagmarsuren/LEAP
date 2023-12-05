f2387985864a50ce9d9f292cc786f0350cefd0a3
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-12-05 19:15:43 +0800

    as

--- methods/index.js
+++ methods/index.js
@@ -1,4 +1,4 @@
-//NEr awah utga
+//ner awah utga
 
 // let name = window.prompt("Tanii ner:");
 // function nameFun(a) {
@@ -37,126 +37,151 @@
 //       break;
 //   }
 
-//   return result;
+//   return;
 // };
 
 // let result = add(Number(x), Number(c));
 
 // console.log(result);
 
-function bodolt() {
-  let tawi, tawanZuut, Amynga, mynga, tawMynga, price, hariult, tawZuut, tawZuu;
+// function bodolt() {
+//   let tawi, tawanZuut, Amynga, mynga, tawMynga, price, hariult, tawZuut, tawZuu;
 
-  // Гараас утга авалт
-  let awahZuil = window.prompt(
-    "Авах барааны урд байгаа тоон утгийг оруулан бараагаа сонгоно уу!\n1: cola = 1200 төгрөг\n2: sprite = 1500 төгрөг\n3: lpenGold = 2000 төгрөг\n4: suu = 2200 төгрөг\n5: alh = 1000 төгрөг\n6: airmag = 500 төгрөг\n7: bohi = 150 төгрөг\n8: arhi = 15000 төгрөг"
-  );
-  let hiisenMongo = window.prompt(
-    "Зөвхөн доорх мөнгөн дэвсгэртээс сонгон оруулна уу!\n5000\n10000\n20000"
-  );
+//   // Гараас утга авалт
+//   let awahZuil = window.prompt(
+//     "Авах барааны урд байгаа тоон утгийг оруулан бараагаа сонгоно уу!\n1: cola = 1200 төгрөг\n2: sprite = 1500 төгрөг\n3: lpenGold = 2000 төгрөг\n4: suu = 2200 төгрөг\n5: alh = 1000 төгрөг\n6: airmag = 500 төгрөг\n7: bohi = 150 төгрөг\n8: arhi = 15000 төгрөг"
+//   );
+//   let hiisenMongo = window.prompt(
+//     "Зөвхөн доорх мөнгөн дэвсгэртээс сонгон оруулна уу!\n5000\n10000\n20000"
+//   );
 
-  // Авах зүйлийн сонголтууд
-  switch (awahZuil) {
-    case "1":
-      price = 1200;
-      break;
-      price = 1500;
-      break;
-    case "3":
-      price = 2000;
-      break;
-    case "4":
-      price = 2200;
-      break;
-    case "5":
-      price = 1000;
-      break;
-    case "6":
-      price = 500;
-      break;
-    case "7":
-      price = 150;
-      break;
-    case "8":
-      price = 15000;
-      break;
-    default:
-      console.log("Тийм бараа байхгүй байна!");
-  }
+//   // Авах зүйлийн сонголтууд
+//   switch (awahZuil) {
+//     case "1":
+//       price = 1200;
+//       break;
+//       price = 1500;
+//       break;
+//     case "3":
+//       price = 2000;
+//       break;
+//     case "4":
+//       price = 2200;
+//       break;
+//     case "5":
+//       price = 1000;
+//       break;
+//     case "6":
+//       price = 500;
+//       break;
+//     case "7":
+//       price = 150;
+//       break;
+//     case "8":
+//       price = 15000;
+//       break;
+//     default:
+//       console.log("Тийм бараа байхгүй байна!");
+//   }
 
-  // Хийсэн мөнгө нь таарч байна уу?
-  switch (hiisenMongo) {
-    case "5000":
-      orsonMongo = 5000;
-      break;
-    case "10000":
-      orsonMongo = 10000;
-      break;
-    case "20000":
-      orsonMongo = 20000;
-      break;
-    default:
-      console.log("Тийм мөнгөн дүн байхгүй");
-      break;
-  }
+//   // Хийсэн мөнгө нь таарч байна уу?
+//   switch (hiisenMongo) {
+//     case "5000":
+//       orsonMongo = 5000;
+//       break;
+//     case "10000":
+//       orsonMongo = 10000;
+//       break;
+//     case "20000":
+//       orsonMongo = 20000;
+//       break;
+//     default:
+//       console.log("Тийм мөнгөн дүн байхгүй");
+//       break;
+//   }
 
-  // Ternary нөхцөл мөнгөн дүн хүрсэн эсэх
-  hiisenMongo <= price
-    ? console.log("Мөнгө хүрсэнгүй! Авах зүйлээс их үнийн дүн оруулна уу!")
-    : (hariult = hiisenMongo - price);
+//   // Ternary нөхцөл мөнгөн дүн хүрсэн эсэх
+//   hiisenMongo <= price
+//     ? console.log("Мөнгө хүрсэнгүй! Авах зүйлээс их үнийн дүн оруулна уу!")
+//     : (hariult = hiisenMongo - price);
 
-  // Бодолт
-  Amynga = parseInt(hariult / 10000);
-  tawMynga = parseInt((hariult % 10000) / 1000);
-  mynga = parseInt((hariult % 10000) / 1000);
-  tawZuu = parseInt((hariult % 1000) / 100);
-  tawi = parseInt((hariult % 100) / 10);
+//   // Бодолт
+//   Amynga = parseInt(hariult / 10000);
+//   tawMynga = parseInt((hariult % 10000) / 1000);
+//   mynga = parseInt((hariult % 10000) / 1000);
+//   tawZuu = parseInt((hariult % 1000) / 100);
+//   tawi = parseInt((hariult % 100) / 10);
 
-  if (tawZuu >= 5) {
-    tawZuut = 1;
-    zuut = tawZuu - 5;
-  } else {
-    zuut = tawZuu;
-    tawZuut = 0;
-  }
-  if (tawMynga >= 5) {
-    mynga = tawMynga - 5;
-    tawMynga = 1;
-  } else {
-    mynga = tawMynga;
-    tawMynga = 0;
-  }
-  if (tawi >= 5) {
-    tawi = 1;
-  } else {
-    tawi = 0;
-  }
+//   if (tawZuu >= 5) {
+//     tawZuut = 1;
+//     zuut = tawZuu - 5;
+//   } else {
+//     zuut = tawZuu;
+//     tawZuut = 0;
+//   }
+//   if (tawMynga >= 5) {
+//     mynga = tawMynga - 5;
+//     tawMynga = 1;
+//   } else {
+//     mynga = tawMynga;
+//     tawMynga = 0;
+//   }
+//   if (tawi >= 5) {
+//     tawi = 1;
+//   } else {
+//     tawi = 0;
+//   }
 
-  //Хариулт хэвлэх
-  if (
-    hiisenMongo >= price &&
-    (hiisenMongo == 5000 || hiisenMongo == 10000 || hiisenMongo == 20000)
-  ) {
-    let niit = Amynga + tawMynga + mynga + tawZuut + zuut + tawi;
-    console.log(
-      Amynga +
-        " Арван мянгат " +
-        tawMynga +
-        " Таван мянгат " +
-        mynga +
-        " Мянгат " +
-        tawZuut +
-        " Таван зуут " +
-        zuut +
-        " зуут " +
-        tawi +
-        " Тавьт " +
-        niit +
-        " Ширхэг дэвсгэрт хариулав: " +
-        " Хариулт нь " +
-        hariult +
-        " Төгрөг"
-    );
-  }
-}
-bodolt();
+//   //Хариулт хэвлэх
+//   if (
+//     hiisenMongo >= price &&
+//     (hiisenMongo == 5000 || hiisenMongo == 10000 || hiisenMongo == 20000)
+//   ) {
+//     let niit = Amynga + tawMynga + mynga + tawZuut + zuut + tawi;
+//     console.log(
+//       Amynga +
+//         " Арван мянгат " +
+//         tawMynga +
+//         " Таван мянгат " +
+//         mynga +
+//         " Мянгат " +
+//         tawZuut +
+//         " Таван зуут " +
+//         zuut +
+//         " зуут " +
+//         tawi +
+//         " Тавьт " +
+//         niit +
+//         " Ширхэг дэвсгэрт хариулав: " +
+//         " Хариулт нь " +
+//         hariult +
+//         " Төгрөг"
+//     );
+//   }
+// }
+// bodolt();
+// let a = prompt("ehnii utga");
+// let b = prompt("hoyr dahi utga");
+// let c = prompt("guraw dahi utga");
+// triangle(Number(a), Number(b), Number);
+// function triangle(par1, par2, par3) {
+//   let cK;
+//   if (a > c && a > b) {
+//     cK = a;
+//     console.log("tegsh ontsogt gurvaljini talbai " + (b * c) / 2);
+//   } else if (c > b && c > a) {
+//     cK = c;
+//     console.log("tegsh ontsogt gurvaljini talbai " + (a * b) / 2);
+//   } else if (b > c && b > a) {
+//     cK = b;
+//     console.log("tegsh ontsogt gurvaljini talbai " + (a * c) / 2);
+//   }
+// }
+
+// let x = prompt("Орлуулах утга аа оруулно уу!");
+// function orluulalt(par1) {
+//   let bodolt = x ** 2 + 5 * x + 6;
+//   return bodolt;
+// }
+// let hariu = orluulalt(x);
+// console.log(hariu);
