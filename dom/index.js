aabf23bb6265e6042c0793e85cb29dd18c2ae817
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-12-19 19:10:01 +0800

    aaaaaa

--- dom/index.js
+++ dom/index.js
@@ -12,3 +12,26 @@
 
 // let a = (document.getElementById("demo").innerHTML = "<p>Pinecone</p>");
 // console.log(a);
+
+// function bgColorChange() {
+//   let d = document.getElementsByClassName("bgColor")[0];
+//   d.style = "background-color: green";
+// }
+// let myButton = document.getElementsByTagName("button")[0];
+
+// myButton.addEventListener("click", bgColorChange);
+
+// 2.
+
+let myButton = document.getElementsByTagName("button");
+let orolt = document.getElementsByTagName("input");
+function asdf() {
+  let davtalt = Number(orolt.value);
+  for (let i = 0; i < davtalt; i++) {
+    let b = document.getElementsByClassName("divClass")[0];
+    b[i].createElement("div");
+    b.style =
+      "width: 50px; height: 50px; border-radius: 50%; background-color: blue;";
+  }
+}
+myButton.addEventListener("click", asdf);
