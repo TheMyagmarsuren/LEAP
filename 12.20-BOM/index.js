d9d7af971efb819b3d768044661e20fd3fb62642
Myagmarsuren Buyanjargal <23LP9283@G401-CC0100.local>
2023-12-21 16:00:41 +0800

    aaaa

--- 12.20-BOM/index.js
+++ 12.20-BOM/index.js
@@ -4,20 +4,43 @@
 // }
 // myButton.addEventListener("click", test);
 
-let b1 = document.getElementsByTagName("button")[0];
-let number = document.getElementsByTagName("input")[0];
-let b2 = document.getElementsByTagName("button")[1];
-number.value = 0;
-function start() {
-  number.value++;
-}
-let interval;
-function a() {
-  let interval = setInterval(start, 1000);
-}
-b1.addEventListener("click", a);
+// let b1 = document.getElementsByTagName("button")[0];
+// let b2 = document.getElementsByTagName("button")[1];
+// let cl = document.getElementsByTagName("button")[2];
+// let number = document.getElementsByTagName("input")[0];
 
-function stop() {
-  clearInterval(interval);
-}
-b2.addEventListener("click", stop);
+// number.value = 0;
+
+// let interval;
+// function start() {
+//    interval = setInterval(()=>{
+//     number.value++;
+//    }, 1000);
+// }
+
+// function stop() {
+//   clearInterval(interval);
+// }
+
+// function reset (){
+//   number.value = 0;
+//   stop();
+// }
+
+// cl.addEventListener("click", reset);
+// b1.addEventListener("click", start);
+// b2.addEventListener("click", stop);
+
+// let sButton = document.getElementsByTagName("button")[0];
+// let pButton = document.getElementsByTagName("button")[1];
+// let time;
+// function openWindow(){
+//   time = setTimeout(()=>{
+//     alert("Booom! ");
+//   }, 2000);
+// }
+// function cancel (){
+//   clearTimeout(time);
+// }
+// sButton.addEventListener("click", openWindow)
+// pButton.addEventListener("click", cancel);
