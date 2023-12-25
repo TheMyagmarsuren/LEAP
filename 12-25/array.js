4ab82600bf8c6296947a41c5cabcb0afdec835c3
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-12-25 19:24:25 +0800

    12-25

---
+++ 12-25/array.js
@@ -0,0 +1,62 @@
+// const array = [1, 2, 3, 4, 5, 6];
+
+// function multi(arr) {
+//   let arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arr2.push(arr[i] * 2);
+//   }
+//   return arr2;
+// }
+// let result = multi(array);
+// console.log(result);
+// const array = [2, 3, 4, 6, 723, 65, 34];
+// function d(arr) {
+//   let arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       arr2.push(arr[i]);
+//     }
+//   }
+//   return arr2;
+// }
+// let result = d(array);
+// console.log(result);
+
+// array.map((el, i) => {
+//   if (el % 2 == 0) {
+//     return console.log(el, i);
+//   }
+// });
+
+// const array = ["cat", "dog", "elephant", "lion"];
+// let a = array.filter((el, i) => el.length >= 4);
+// console.log(a);
+
+// const array = [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }];
+
+// let result = array.map((el) => {
+//   return el.name;
+// });
+// console.log(result);
+
+// const array = [1, "apple", true, 42, "banana"];
+
+// let result = array.filter((el) => typeof el === "string");
+// console.log(result);
+
+// const array = [2, 4, 6];
+// let result = array.map((el) => el * el);
+// console.log(result);
+
+// const array = [1, 2, 2, 3, 4, 4, 5];
+
+// let result = array.filter((el, index, arr) => {});
+// console.log(result);
+
+// const array = ["a", "b", "c"];
+// const array2 = [1, 2, 3];
+
+// let result = array.map((el, i) => {
+//   return el + "-" + array2[i];
+// });
+// console.log(result);
