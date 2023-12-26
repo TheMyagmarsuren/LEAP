e5da041b3c1f69191181329814791b0a426a442e
Myagmarsuren Buyanjargal <23LP9283@G301-CC0132.local>
2023-12-26 19:28:03 +0800

    12-26

---
+++ 12-26-drag/index.js
@@ -0,0 +1,7 @@
+const boxArray = ["Adiya", "Dulmaa", "Bat", "Dondog", "Zulaa", "Nomin"];
+let sum = "";
+let space = " ";
+for (let i = 0; i < boxArray.length; i++) {
+  sum += boxArray[i] + " " + space;
+}
+console.log(sum);
--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,9 +1,3 @@
 let addBtn = document.getElementsByClassName("add-btn")[0];
-function add() {
-  let id = document.getElementById("toDo");
-  let divCreate = document.createElement("div");
-
-  let myWindow = open("https://www.youtube.com/watch?v=hpJUuFpBsJA");
-  id.appendChild(myWindow);
-}
+function add() {}
 addBtn.addEventListener("click", add);
